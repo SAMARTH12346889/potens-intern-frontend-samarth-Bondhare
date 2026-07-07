0699eedafb7a578a6fe889e2179bd6ff6a24c39e
SAMARTH123456889 <sambondhare123@gmail.com>
2026-07-07 19:58:33 +0530

    covert into pwa

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,8 +1,60 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-})
+  plugins: [
+    react(),tailwindcss(),visualizer({
+      open: true,
+    }),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      includeAssets: [
+        "favicon.svg",
+        "apple-touch-icon.png",
+        "masked-icon.svg"
+      ],
+
+      manifest: {
+        name: "Citizen Complaint App",
+        short_name: "Complaint",
+
+        description: "Progressive Web App for reporting issues",
+
+        theme_color: "#4f46e5",
+        background_color: "#ffffff",
+
+        display: "standalone",
+
+        start_url: "/",
+
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ]
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,jpg,jpeg,svg,woff2}"]
+      }
+    })
+  ]
+});
